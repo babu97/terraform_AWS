1511d8adbfedfef583e93f8bb74360fe1a9c5900
babu97 <benkaimugul@gmail.com>
2023-12-28 02:15:15 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+    region = var.region
+  
+}
+#create vpc 
+resource "aws_vpc" "main" {
+
+    cidr_block = var.vpc_cidr
+    enable_dns_support =var.enable_dns_support
+    enable_dns_hostnames =var.enable_dns_hostnames
+ 
+}
+#create public subnets1
+
+
+resource "aws_subnet" "public_subnet" {
+  count                    = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+  vpc_id                   = aws_vpc.main.id
+  cidr_block               = cidrsubnet(var.vpc_cidr, 4, count.index)
+  map_public_ip_on_launch  = true
+  availability_zone        = data.aws_availability_zones.available.names[count.index]
+}
+
+#create public subnet2
+
+
+resource "aws_subnet" "private_subnet" {
+    count =  var.preferred_number_of_private_subnets ==null ? length(data.aws_availability_zones.available.names): var.preferred_number_of_private_subnets
+    vpc_id = aws_vpc.main.id
+    cidr_block = cidrsubnet(var.vpc_cidr, 5,count.index)
+    map_public_ip_on_launch = false
+    availability_zone = data.aws_availability_zones.available.names[count.index]
+
+}
+
+#Get list of availability zones
+
+data "aws_availability_zones" "available" {
+    state = "available"
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  
+  default = "eu-central-1"
+}
+variable "vpc_cidr" {
+    default = "172.16.0.0/16"
+  
+}
+variable "enable_dns_support" {
+    default = "true"
+  
+}
+variable "enable_dns_hostnames" {
+    default = "true"
+  
+}
+
+variable "preferred_number_of_public_subnets" {
+default=null
+}
+variable "preferred_number_of_private_subnets" {
+    default = null
+  
+}
