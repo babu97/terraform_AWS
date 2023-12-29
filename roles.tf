435ac3f2178804c44d927df10568ef08da4928b4
babu97 <benkaimugul@gmail.com>
2023-12-29 14:25:21 +0000

    finished creating and assigning roles

---
+++ roles.tf
@@ -0,0 +1,62 @@
+resource "aws_iam_role" "ec2_instance_role" {
+name = "ec2_instance_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+    tags = merge(
+    var.tags,
+    {
+      Name = "aws assume role"
+    },
+  )
+}
+
+
+resource "aws_iam_policy" "policy" {
+    name = "ec2_instance_policy"
+    description = "A test policy"
+    policy = jsondecode({
+        Version = "2012-10-17", 
+        Statement=[
+            {
+                Action = [
+                    "ec2:Describe*",
+                ]
+                Effect = "Allow"
+                resource = "*"
+            },
+        ]
+    })
+  
+    tags = merge(
+    var.tags,
+    {
+      Name =  "aws assume policy"
+    },
+  )
+
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+    role = aws_iam_role.ec2_instance_role.name
+    policy_arn = aws_iam_policy.policy.arn
+
+  
+}
+
+resource "aws_iam_instance_profile" "ip" {
+    name = "aws_instance_profile_test"
+    role = aws_iam_role.ec2_instance_role.name
+  
+}
+
